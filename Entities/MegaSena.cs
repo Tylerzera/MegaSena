14ef3e7e37596269d3fddc0d7239a648e51caa01
Tylerzeraa <nathankz2@hotmail.com>
2023-09-23 20:51:29 -0300

    Adicionar arquivos de projeto.

---
+++ Entities/MegaSena.cs
@@ -0,0 +1,33 @@
+﻿using MegaSena.Atributos;
+using System.ComponentModel.DataAnnotations;
+
+namespace MegaSena.Entities
+{
+    public class MegaSena
+    {
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+        public int PrimeiroNro { get; set; }
+        public int SegundoNro { get; set; }
+        public int TerceiroNro { get; set; }
+        public int QuartoNro { get; set; }
+        public int QuintoNro { get; set; }
+        public int SextoNro { get; set; }
+        public DateTime DataJogo { get; set; }
+
+        public bool NumerosValidos()
+        {
+            if (PrimeiroNro == SegundoNro || PrimeiroNro == TerceiroNro || PrimeiroNro == QuartoNro || PrimeiroNro == QuintoNro || PrimeiroNro == SextoNro ||
+                SegundoNro == TerceiroNro || SegundoNro == QuartoNro || SegundoNro == QuintoNro || SegundoNro == SextoNro ||
+                TerceiroNro == QuartoNro || TerceiroNro == QuintoNro || TerceiroNro == SextoNro ||
+                QuartoNro == QuintoNro || QuartoNro == SextoNro ||
+                QuintoNro == SextoNro)
+                return false;
+            return true;
+        }
+        public void atualizarData()
+        {
+            DataJogo = DateTime.Now;
+        }
+    }
+}
---
+++ Models/MegaSenaViewModel.cs
@@ -0,0 +1,28 @@
+﻿using MegaSena.Atributos;
+using System.ComponentModel.DataAnnotations;
+
+namespace MegaSena.Models
+{
+    public class MegaSenaViewModel
+    {
+        [CustomStringLengthValidation(5, 255)]
+        public string Nome { get; set; }
+
+        [CPFValidation]
+        public string Cpf { get; set; }
+
+        [Range(1, 60)]
+        public int PrimeiroNro { get; set; }
+        [Range(1, 60)]
+        public int SegundoNro { get; set; }
+        [Range(1, 60)]
+        public int TerceiroNro { get; set; }
+        [Range(1, 60)]
+        public int QuartoNro { get; set; }
+        [Range(1, 60)]
+        public int QuintoNro { get; set; }
+        [Range(1, 60)]
+        public int SextoNro { get; set; }
+        public DateTime DataJogo { get; set; }
+    }
+}
